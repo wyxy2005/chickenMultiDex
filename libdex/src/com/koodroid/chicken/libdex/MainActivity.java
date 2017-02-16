4724f58827f5132007ce170075bf8d0dcb8290af
dingchao <daniel.dingchao@gmail.com>
2017-02-16 13:44:23 +0800

    init project

---
+++ libdex/src/com/koodroid/chicken/libdex/MainActivity.java
@@ -0,0 +1,15 @@
+package com.koodroid.chicken.libdex;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.demo1_activity_main);
+    }
+
+
+}
